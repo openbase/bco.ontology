9aa144f28c76101e0f7421e5fd5496ac1fbc9fa6
Divine Threepwood <divine@openbase.org>
2018-03-31 21:32:55 +0200

    header updates applied

--- lib/src/main/java/org/openbase/bco/ontology/lib/trigger/sparql/AskQueryExample.java
+++ lib/src/main/java/org/openbase/bco/ontology/lib/trigger/sparql/AskQueryExample.java
@@ -1,22 +1,26 @@
-/**
- * ==================================================================
- *
- * This file is part of org.openbase.bco.ontology.lib.
- *
- * org.openbase.bco.ontology.lib is free software: you can redistribute it and modify
- * it under the terms of the GNU General Public License (Version 3)
- * as published by the Free Software Foundation.
- *
- * org.openbase.bco.ontology.lib is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with org.openbase.bco.ontology.lib. If not, see <http://www.gnu.org/licenses/>.
- * ==================================================================
- */
 package org.openbase.bco.ontology.lib.trigger.sparql;
+
+/*-
+ * #%L
+ * BCO Ontology Library
+ * %%
+ * Copyright (C) 2016 - 2018 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 
 import org.openbase.bco.ontology.lib.trigger.Trigger;
 import org.openbase.bco.ontology.lib.trigger.TriggerFactory;
